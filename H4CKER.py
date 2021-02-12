916f2d0acb2e45b787892f7aaf6e50c243effca2
boss-ki <77185405+boss-ki@users.noreply.github.com>
2021-02-12 09:01:54 +0530

    https://github.com/boss-ki/beta-H4CKER.git
    https://github.com/boss-ki/beta-H4CKER.git
    All in one hacking Tool

---
+++ H4CKER.py
@@ -0,0 +1,34 @@
+"""""
+HACKING TOOL
+"""""
+print("                  DEVELOPED BY H4CKER")
+print("                     MADE IN INDIA")
+name = input("ENETER YOUR NAME : ")
+print("\n")
+age = input("ENTER YOUR AGE : ")
+
+print("\n")
+print("WELCOME" , name )
+print("\n")
+print(" ____________________ ")
+print("POWERFUL TERMUX TOOLS")
+print(" _____________________")
+print("\n")
+
+print("[01] NEXPHISHER => POWERFUL PHISHING TOOL")
+
+print("\n")
+print("JUST COPY AND PASTE ON YOUR TERMUX")
+
+print(" 👇")
+print("\n")
+print("apt update && apt upgrade && pkg install git && cd $HOME && git clone git://github.com/htr-tech/nexphisher.git && cd nexphisher && bash tmux_setup && bash nexphisher")
+print("\n")
+
+print("_________________________")
+print(" TOOL COMING SOON ")
+print("_________________________")
+
+print("\n")
+print("THANKS")
+print("__________")
