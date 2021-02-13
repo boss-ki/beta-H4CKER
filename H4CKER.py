1135361dc2dcd00a97fe1972654a1a9c27b58917
boss-ki <77185405+boss-ki@users.noreply.github.com>
2021-02-13 12:23:19 +0530

    Add files via upload

--- H4CKER.py
+++ H4CKER.py
@@ -1,34 +1,116 @@
-"""""
+""""
 HACKING TOOL
 """""
-print("                  DEVELOPED BY H4CKER")
-print("                     MADE IN INDIA")
+import os
+os.system('clear')
+import os
+stream = os.popen('echo Returned output')
+output = stream.read()
+output
+
+import sys
+
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint 
+from pyfiglet import figlet_format
+
+cprint(figlet_format('H4CKER', font='starwars'),
+        attrs=['bold'])
+        
+print("                  DEVELOPED BY ANONYMOUS HACKER")
+print("\n")
+
+cprint(figlet_format('TOOL', font='starwars'),
+        attrs=['bold'])
+        
 name = input("ENETER YOUR NAME : ")
 print("\n")
-age = input("ENTER YOUR AGE : ")
 
-print("\n")
+
+
 print("WELCOME" , name )
 print("\n")
-print(" ____________________ ")
-print("POWERFUL TERMUX TOOLS")
-print(" _____________________")
+print("                  ______________________ ")
+print("                   POWERFUL TERMUX TOOLS")
+print("                   _____________________")
 print("\n")
 
 print("[01] NEXPHISHER => POWERFUL PHISHING TOOL")
 
 print("\n")
 print("JUST COPY AND PASTE ON YOUR TERMUX")
+print("\n")
 
 print(" 👇")
 print("\n")
 print("apt update && apt upgrade && pkg install git && cd $HOME && git clone git://github.com/htr-tech/nexphisher.git && cd nexphisher && bash tmux_setup && bash nexphisher")
 print("\n")
 
-print("_________________________")
-print(" TOOL COMING SOON ")
-print("_________________________")
+print("[02] SQLMAP => POWERFULL TOOL FOR AUTOMATIC SQL INJECTION AND DATABASE TAKEOVER TOOL")
 
 print("\n")
-print("THANKS")
-print("__________")
+
+print("JUST COPY AND PASTE ON YOUR TERMUX ")
+print("\n")
+print(" 👇")
+
+print("\n")
+
+print(" apt update && apt upgrade && pkg install git && pkg install python && pkg install python2 && cd $HOME && git clone https://github.com/sqlmapproject/sqlmap.git && cd sqlmap && python2 sqlmap.py ")
+
+
+print("\n")
+print("[03] METASPLOIT => POWERFULL TOOL FOR GENERATING PAYLOAD AND TO PROBE SYSTEMATIC VULNERABILITIES ON NETWORK AND SERVER")
+
+print("\n")
+print("JUST COPY AND PASTE ON YOUR TERMUX")
+print("\n")
+
+
+print(" 👇")
+print("\n")
+
+print("pkg update && pkg upgrade && cd $HOME && pkg install git curl wget nmap -y && curl -LO raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh && chmod 777 metasploit.sh && ./metasploit.sh" )
+print("\n\n")
+
+print("____________________")
+print("    COMIG SOON")
+print("_____________________")
+
+print("TO UPDATE PACKAGES 👇 ")
+print("\n")
+A = input( "Y or N : ")
+
+if A == 'Y' or A == 'y' :
+	os.system(' pkg update')
+import os
+stream = os.popen('echo Returned output')
+output = stream.read()
+output
+
+if A == 'N' or 'n' :
+	print("\n")
+	print("THANKS FOR USING OUR TOOL IT'S UNDER MAINTAINANCE ... Coming Soon....")
+	print("\n")
+B = input("TO INSTALL ALL TOOLS PRESS Y or N : ")
+	
+if B == 'Y' or B == 'y' :
+	print("/n")
+	os.system('apt update && apt upgrade && pkg install git && cd $HOME && git clone git://github.com/htr-tech/nexphisher.git && cd nexphisher && bash tmux_setup && bash nexphisher')
+import os
+stream = os.popen('echo Returned output')
+output = stream.read()
+output
+
+if  B == 'N' or B == 'n' :
+	print("THANKS FOR USING OUR TOOL IT'S UNDER MAINTAINANCE ... Coming Soon....")
+
+
+	
+
+
+
+
+
+
